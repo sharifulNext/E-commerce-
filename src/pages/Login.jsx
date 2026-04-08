56adb2bcb74649f98a7ee7a1c612148208ec217d
Shariful Islam <sharifulislam242248@gmail.com>
2026-04-08 13:40:55 +0600

    fix done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -56,15 +56,7 @@ const Login = () => {
 
   },[token])
 
-  const handleGoogleLogin = async () => {
-    try {
-      const result = await signInWithPopup(auth, provider);
-      console.log("User logged in:", result.user);
-    } catch (error) {
-      console.error("Error signing in with Google:", error.message);
-    }
-  };
-
+ 
   return (
     <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:w-[400px] m-auto mt-14 gap-4 text-gray-800 bg-white p-6 shadow-lg rounded-lg'>
       {/* Title */}
@@ -90,18 +82,6 @@ const Login = () => {
 
       <button type="submit" className='w-full bg-black text-white py-2 rounded mt-2 hover:bg-gray-800 transition'>{currentState}</button>
 
-      {/* Google Login Divider */}
-      <div className="flex items-center w-full my-2">
-        <hr className="flex-1 border-gray-300" />
-        <span className="px-2 text-xs text-gray-500">OR</span>
-        <hr className="flex-1 border-gray-300" />
-      </div>
-
-      {/* Google Login Button */}
-      <button type="button" onClick={handleGoogleLogin} className='w-full flex items-center justify-center gap-2 bg-white border border-gray-300 py-2 rounded hover:bg-gray-50 transition'>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg" alt="Google" className='w-5' />
-        Continue with Google
-      </button>
     </form>
   );
 };
